1c719fc32da29b8d033337579a7c012b92e6409b
datboyblu3 <datboyblu3@protonmail.com>
2024-07-19 12:54:54 -0400

    added oci_identity_group in 0-providers.tf file

--- 0-providers.tf
+++ 0-providers.tf
@@ -11,3 +11,14 @@ provider "oci" {
   # Configuration options
   feature{}
 }
+
+resource "oci_identity_group" "test_group" {
+    #Required
+    compartment_id = var.tenancy_ocid
+    description = var.group_description
+    name = var.group_name
+
+    #Optional
+    defined_tags = {"Operations.CostCenter"= "42"}
+    freeform_tags = {"Department"= "Finance"}
+}
---
+++ 1-networks.tf
@@ -0,0 +1,7 @@
+######################################################################################
+# Create a Network for Wazuh to Operate In the Selecte Resource Group.
+# This will also create a Subnet into the Virtual Network.
+# The subnet will be deployed separately from the Virtual Network for ease of 
+# maintenance.
+######################################################################################
+
