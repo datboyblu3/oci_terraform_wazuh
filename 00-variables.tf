0cab897c8f795ca8529996b75fc3ee6b9823086e
datboyblu3 <datboyblu3@protonmail.com>
2024-08-12 15:18:45 -0700

    created variables.tf

---
+++ 00-variables.tf
@@ -0,0 +1,23 @@
+variable "compartment_id" {
+
+}
+
+variable "public_route_display_name"{
+
+}
+
+variable "private_route_display_name"{
+
+}
+
+variable "internet_gateway_display_name"{
+
+}
+
+variable "nat_gateway_display_name"{
+
+}
+
+variable "internet_gateway_enabled"{
+
+}
