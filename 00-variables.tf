a19c7c95a2ba3bff1d44c915fdbcb1e9a0cd0f8c
datboyblu3 <datboyblu3@protonmail.com>
2024-09-10 11:51:21 -0700

    cleaned up 00-variables.tf script

--- 00-variables.tf
+++ 00-variables.tf
@@ -1,23 +1,33 @@
 variable "compartment_id" {
-
+    description = "compartment id"
+    type = string
 }
 
 variable "public_route_display_name"{
-
+    description = "public-route-table"
+    type        = string
+    default     = "public-route-table"
 }
 
 variable "private_route_display_name"{
-
+    description = "private-route-table"
+    type        = string
+    default     = "private-route-table"
 }
 
 variable "internet_gateway_display_name"{
-
+    description = "test-vcn-igw"
+    type        = string
+    default     = "internet-gateway"
 }
 
 variable "nat_gateway_display_name"{
-
+    description = "test-vcn-ng"
+    type        = string
+    default     = "nat-gateway"
 }
 
 variable "internet_gateway_enabled"{
-
+    type        = bool
+    defeault    = true
 }
